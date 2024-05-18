9f148bafdb8b557647443b7ed11eb8c6b8a71292
Harizibam7 <harizibamv@gmail.com>
2024-05-18 19:18:12 +0530

    updated

--- src/components/about-hero.tsx
+++ src/components/about-hero.tsx
@@ -32,7 +32,7 @@ export default function AboutHero() {
           </FadeUp>
           <FadeUp key="description-1" duration={0.6} delay={0.2}>
             <p className="mt-8 text-base font-medium text-zinc-900 dark:text-zinc-300 sm:text-lg md:text-lg">
-                An 18 year guy interested in Full Stack and Artificial Intelligence. I do both development and deployment. To know more follow me at github, LinkedIn and Twitter. 
+            An·18·year·guy·interested·in·Full·Stack·and·Artificial⏎··············Intelligence.·I·do·both·development·and·deployment.·To·know·more⏎··············follow·me·at·github,·LinkedIn·and·Twitter.
             </p>
           </FadeUp>
           <FadeUp key="description-2" duration={0.6} delay={0.4}>
